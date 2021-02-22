f17d5c90acb9e8f2868d45376dbcad58fe7046b9
HAACKKKER MAAANNNNNN <retronbv@gmail.com>
2021-02-22 17:20:50 +0000

    switch to new version of retronhosting

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
-const express = require('express');
-const fs = require('fs'); 
-const app = express();
-
-const data = JSON.parse(fs.readFileSync('./config.retronhost.js', {encoding:'utf8', flag:'r'}));
-
-app.get('*', (req, res) => {
-  url = ""
-  if (data.pages[req.originalUrl] === undefined) {
-    console.log("404 "+req.originalUrl)
-    url = data["404"]
-  } else {
-    console.log("200 "+req.originalUrl)
-    url = data.pages[req.originalUrl]
-  }
-  content = fs.readFileSync(url, {encoding:'utf8', flag:'r'})
-  res.send(content)
-});
-
-app.listen(3000, () => {
-  console.log('server started');
-  console.log(data)
-});
+const retron = require('retronhosting');
+retron.setup("./config.retronhosting.js")
